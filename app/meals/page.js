e3940ef44059b7a813067c3bab6710a590948d6c
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-07-11 11:38:02 +0200

    Update Meals Page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,44 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '../../components/meals/MealsGrid';
+import { getMeals } from '../../lib/meals';
+import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
+export const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
 const MealsPage = () => {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Loading Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
