2fc51656c993503eb3d1574ad2da4c347ebee82c
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-07-11 11:35:39 +0200

    Create not-found.js

---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal does not exist.</h1>
+      <p>Unfortunately, we could not find the page of requested meal.</p>
+    </main>
+  );
+};
+
+export default NotFound;
